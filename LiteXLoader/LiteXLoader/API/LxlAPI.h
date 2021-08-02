cc7e202ec6c26fdd19b5cb57eeb6e01ae1190fb4
Qi You <1425321705@qq.com>
2021-08-02 12:08:54 +0800

    change version system

--- LiteXLoader/LiteXLoader/API/LxlAPI.h
+++ LiteXLoader/LiteXLoader/API/LxlAPI.h
@@ -3,6 +3,7 @@
 using namespace script;
 
 Local<Value> LxlGetVersion(const Arguments& args);
+Local<Value> LxlCheckVersion(const Arguments& args);
 Local<Value> LxlListPlugins(const Arguments& args);
 Local<Value> LxlRequire(const Arguments& args);
 Local<Value> LxlEval(const Arguments& args);
--- LiteXLoader/LiteXLoader/API/StaticClasses.h
+++ LiteXLoader/LiteXLoader/API/StaticClasses.h
@@ -230,6 +230,7 @@ class LxlClass
 {
 public:
     static Local<Value> version(const Arguments& args) { return LxlGetVersion(args); }
+    static Local<Value> checkVersion(const Arguments& args) { return LxlCheckVersion(args); }
     static Local<Value> listPlugins(const Arguments& args) { return LxlListPlugins(args); }
     static Local<Value> importFunc(const Arguments& args) { return LxlImport(args); }
     static Local<Value> exportFunc(const Arguments& args) { return LxlExport(args); }
@@ -240,6 +241,7 @@ public:
 static ClassDefine<void> LxlClassBuilder =
     defineClass("lxl")
         .function("version", &LxlClass::version)
+        .function("checkVersion", &LxlClass::checkVersion)
         .function("listPlugins", &LxlClass::listPlugins)
         .function("import", &LxlClass::importFunc)
         .function("export", &LxlClass::exportFunc)
