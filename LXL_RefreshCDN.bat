1867d00a57e1651813e08f82faca0c077a3fec3c
Qi You <1425321705@qq.com>
2021-08-04 19:06:49 +0800

    Create LXL_RefreshCDN.bat

---
+++ LXL_RefreshCDN.bat
@@ -0,0 +1,4 @@
+@echo off
+start https://purge.jsdelivr.net/gh/LiteLDev/update/LXL.json
+start https://purge.jsdelivr.net/gh/LiteLDev/update/LXL/LiteXLoader.Js.dll
+start https://purge.jsdelivr.net/gh/LiteLDev/update/LXL/LiteXLoader.Lua.dll
