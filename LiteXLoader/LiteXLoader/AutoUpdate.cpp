b77614035b36c549d9d63ef00fb8cba1d0c9c90e
yqs112358 <1425321705@qq.com>
2021-08-09 22:58:45 +0800

    fix request bug

--- LiteXLoader/LiteXLoader/AutoUpdate.cpp
+++ LiteXLoader/LiteXLoader/AutoUpdate.cpp
@@ -23,8 +23,7 @@ using namespace std;
 void SetHttpHeader(httplib::Client* cli)
 {
 	cli->set_default_headers({
-		{"cache-control", "max-age=0"},
-		{ "Accept-Encoding", "gzip, deflate, br" },
+		{ "cache-control", "max-age=0" },
 		{ "UserAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67" }
 	});
 }
