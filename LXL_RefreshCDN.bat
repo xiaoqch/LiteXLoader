83875a9e3dd22376f243b588e4075e5264a45b6a
yqs112358 <1425321705@qq.com>
2021-08-16 09:55:30 +0800

    Delete LXL_RefreshCDN.bat

--- LXL_RefreshCDN.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start https://purge.jsdelivr.net/gh/LiteLDev/update@master/LXL.json
-start https://purge.jsdelivr.net/gh/LiteLDev/update@master/LXL/LiteXLoader.Js.dll
-start https://purge.jsdelivr.net/gh/LiteLDev/update@master/LXL/LiteXLoader.Lua.dll
