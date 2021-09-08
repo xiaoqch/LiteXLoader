09740f82d288316a4bc98cfeb2693ebaf2f01adf
wzy <59381521+wzyyyyyyy@users.noreply.github.com>
2021-09-08 12:44:54 +0800

    fix something
    fix something

--- LiteXLoader/LiteXLoader/Kernel/Base.h
+++ LiteXLoader/LiteXLoader/Kernel/Base.h
@@ -1,8 +1,10 @@
 #include <string>
 
+class Level;
+class Dimension;
 bool Raw_Runcmd(const std::string &cmd);
 std::pair<bool, std::string> Raw_RuncmdEx(const std::string &cmd);
 bool Raw_RegisterCmd(const std::string &cmd, const std::string &describe, int cmdLevel);
 bool Raw_SendCmdOutput(const std::string& output);
 std::string Raw_GetBDSVersion();
-void* Raw_GetDimByLevel(Level* lv, int id);
+Dimension* Raw_GetDimByLevel(Level* lv, int id);
